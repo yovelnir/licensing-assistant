9d3456470731f8635d932d86c4eddc23c5cd61a4
YovelNir <yovelnir@gmail.com>
2025-10-27 17:30:42 +0200

    expose backend app

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,10 @@ import os
 from dotenv import load_dotenv
 from app import create_app
 
+app = create_app()
 
 def main() -> None:
     load_dotenv()
-    app = create_app()
     port = int(os.getenv("PORT", "5000"))
     app.run(host="0.0.0.0", port=port, debug=True)
 
